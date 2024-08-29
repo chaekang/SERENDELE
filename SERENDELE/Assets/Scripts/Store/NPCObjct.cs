ade4cb3857e5386caa6c2f882162c821e812e41d
chaekang <herry612@naver.com>
2024-08-30 02:08:25 +0900

    [PLAYER/PREF] E키로 상호작용 통일
    * NPC 대화, 창고 모두 E키로 통일

--- SERENDELE/Assets/Scripts/Store/NPCObjct.cs
+++ SERENDELE/Assets/Scripts/Store/NPCObjct.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEditor.Progress;
 
-public class NPCObjct : MonoBehaviour, IInteractable
+public class NPCObjct : MonoBehaviour, INPCInteractable
 {
     public NPCData npcData;
     public string GetInteractPrompt()
@@ -14,6 +14,104 @@ public class NPCObjct : MonoBehaviour, IInteractable
 
     public void OnInteract()
     {
-        Debug.Log("NPC 대화");
+
+    }
+
+    public void HandleNPCInteraction(GameObject curInteractGameobject)
+    {
+        if (curInteractGameobject.CompareTag("MarketNPC"))
+        {
+            Choice choiceData = curInteractGameobject.GetComponent<Choice>();
+
+            if (choiceData != null)
+            {
+                ChoiceManager.Instance.ShowChoice(choiceData);
+                StartCoroutine(WaitForMarketChoice());
+            }
+        }
+        else if (curInteractGameobject.CompareTag("HotelNPC"))
+        {
+            Choice choiceData = curInteractGameobject.GetComponent<Choice>();
+
+            if (choiceData != null)
+            {
+                ChoiceManager.Instance.ShowChoice(choiceData);
+                StartCoroutine(WaitForHotelChoice());
+            }
+        }
+        else if (curInteractGameobject.CompareTag("NPC"))
+        {
+            TalkManager.Instance.Action(curInteractGameobject);
+        }
+    }
+
+    private IEnumerator WaitForMarketChoice()
+    {
+        // 사용자 입력 대기
+        while (ChoiceManager.Instance.choiceIng)
+        {
+            yield return null;
+        }
+
+        // 사용자가 H 키를 누를 때까지 대기
+        while (!Input.GetKeyDown(KeyCode.H))
+        {
+            yield return null;
+        }
+
+        // 선택 결과에 따른 행동 처리
+        int result = ChoiceManager.Instance.GetResult();
+
+        switch (result)
+        {
+            case 1:
+                GameManager.Instance.MarketManager.BuyPanel.SetActive(true);
+                Debug.Log("Result: " + result);
+                break;
+            case 2:
+                GameManager.Instance.MarketManager.SellPanel.SetActive(true);
+                Debug.Log("Result: " + result);
+                break;
+            case 3:
+                Debug.Log("Result: " + result);
+                break;
+            default:
+                break;
+        }
+
+        ChoiceManager.Instance.ExitChoice();
+    }
+
+    private IEnumerator WaitForHotelChoice()
+    {
+        // 사용자 입력 대기
+        while (ChoiceManager.Instance.choiceIng)
+        {
+            yield return null;
+        }
+
+        // 사용자가 H 키를 누를 때까지 대기
+        while (!Input.GetKeyDown(KeyCode.H))
+        {
+            yield return null;
+        }
+
+        // 선택 결과에 따른 행동 처리
+        int result = ChoiceManager.Instance.GetResult();
+
+        switch (result)
+        {
+            case 1:
+                MoneyManager.Spend(50);
+                Debug.Log("Result: " + result);
+                break;
+            case 2:
+                Debug.Log("Result: " + result);
+                break;
+            default:
+                break;
+        }
+
+        ChoiceManager.Instance.ExitChoice();
     }
 }
